e5e528e2ef03fd6f3fbbbc800a8ec99a7c710e16
Mar1nbawe <vladcostinmc@yahoo.ro>
2025-01-08 13:22:50 +0200

    modifying interprettor and adding old one

--- Interpretter.py
+++ Interpretter.py
@@ -1,43 +1,10 @@
-# List of actions
-actions = ["Help", "Fight", "Steal"]
 
-# Dictionary of synonyms for each action
-synonyms_dict = {
-    "Help": ["Assist", "Aid", "Support", "Rescue", "Serve"],
-    "Fight": ["Battle", "Combat", "Brawl", "Clash", "Struggle"],
-    "Steal": ["Rob", "Thieve", "Pilfer", "Swipe", "Snatch"]
-}
+from transformers import pipeline
 
+def interpretPipeline(sentence):
+    pipe = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
+    return pipe(sentence)
 
-# Function to get the action from the user
-def get_action():
-    # Get the action from the user
-    action = "The peasant assists the knight in his quest."
-
-    # Check if the action is valid
-    for key, synonyms in synonyms_dict.items():
-        if any(word.lower() in action.lower() for word in [key] + synonyms):
-            print(f"Action {action} is valid. Matches with {key} / {synonyms}")
-            return key
-    print("The action is invalid.")
-    return None
-
-
-# get_action()
-
-
-import spacy
-
-
-# Load the spaCy model
-nlp = spacy.load("en_core_web_trf")
-
-# Sentence to analyze
-sentence = "The peasant assists the knight in his quest."
-
-# Process the sentence
-doc = nlp(sentence)
-
-synonym_exists = any(token.lemma_.lower() in [syn.lower() for syn in synonyms_dict] for token in doc)
-
-print(synonym_exists)
+if __name__ == "__main__":
+    result = interpretPipeline("Alex is the worst. Really now?")
+    print(result)
